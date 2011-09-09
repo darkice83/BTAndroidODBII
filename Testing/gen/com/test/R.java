f9bd0283e1dce450219fd0aa25b0a07f4b06c100
Shawn Guillemette <darkice83@gmail.com>
2011-09-09 17:54:59 -0400

    Sending Bluetooth commands using the ui thread to bt device "CHX"
    Signed-off-by: Shawn Guillemette <darkice83@gmail.com>

--- Testing/gen/com/test/R.java
+++ Testing/gen/com/test/R.java
@@ -15,6 +15,8 @@ public final class R {
     }
     public static final class id {
         public static final int enumerate=0x7f050000;
+        public static final int exit=0x7f050001;
+        public static final int textlog=0x7f050002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- Testing/src/com/test/TestingActivity.java
+++ Testing/src/com/test/TestingActivity.java
@@ -1,17 +1,26 @@
 package com.test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.UUID;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class TestingActivity extends Activity {
 	private BluetoothAdapter bAdapter;
+	private BluetoothDevice bDev;
+	private UUID uid = UUID.fromString("bd302500-d594-11e0-9572-0800200c9a66");
+	public final static UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 	
 	/** Called when the activity is first created. */
     @Override
@@ -21,6 +30,7 @@ public class TestingActivity extends Activity {
         Button enumerate = (Button) findViewById(R.id.enumerate);
         enumerate.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
+				TextView logarea = (TextView) findViewById(R.id.textlog);
 				bAdapter = BluetoothAdapter.getDefaultAdapter();
 				Set<BluetoothDevice> bDevices = bAdapter.getBondedDevices();
 				Iterator<BluetoothDevice> i = bDevices.iterator();
@@ -32,9 +42,36 @@ public class TestingActivity extends Activity {
 					if (chx.getName().equals( "CHX")){
 						System.out.println("Found CHX");
 					}
+					bDev = chx;
+					Button enumerate = (Button) findViewById(R.id.enumerate);
+					enumerate.setText("Found CHX");
+					enumerate.setEnabled(false);
 					
-
 				}
+				logarea.append(" " + bDev.getBondState());
+				try {
+					logarea.append("trying to create socket");
+					BluetoothSocket socket = bDev.createRfcommSocketToServiceRecord(uuid);
+					logarea.append("socket created? trying to connect");
+					socket.connect();
+					logarea.append("connected?, getting OStrean,IStream and trying to send");
+					OutputStream os = socket.getOutputStream();
+					InputStream is = socket.getInputStream();
+					os.write(0);
+					logarea.append("done i guess");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					logarea.append("CARP");
+					System.out.println("can't create RF Comm Socket");
+				}	
+			}
+        });
+        Button exit = (Button) findViewById(R.id.exit);
+        exit.setOnClickListener(new View.OnClickListener(){
+
+			public void onClick(View v) {
+				finish();
 			}
         });
     }
